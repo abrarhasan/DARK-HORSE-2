04bb7c7e81a368e9daf1c793c1618d6db55d61a2
Abrar Hasan Aadil <mdhojaifa550@gmail.com>
2026-01-16 21:39:17 +0600

    Update info.js

--- scripts/cmds/info.js
+++ scripts/cmds/info.js
@@ -1,74 +1,44 @@
-const moment = require("moment-timezone");
-
-module.exports = {
   config: {
     name: "info",
     version: "2.5.3",
-    author: "ABRAR",
+    author: "ST | Sheikh Tamim",
     role: 0,
     countDown: 20,
     shortDescription: {
-      en: "Owner & bot information"
-    },
-    longDescription: {
-      en: "Show detailed information about the bot, owner, uptime and socials"
-    },
-    category: "owner",
-    guide: {
-      en: "{pn}"
-    }
-  },
+@@ -21,10 +21,10 @@ module.exports = {
 
   onStart: async function ({ message }) {
 
     const ownerName = "ABRAR HASAN";
     const ownerAge = "22";
     const ownerFB = "https://www.facebook.com/abrar.hasan.125760550";
-    const ownerNumber = "https://m.me/abrar.hasan.125760550";
+    const ownerNumber = "+88018XXXXXXX";
     const status = "Active";
 
     const botName = global.GoatBot?.config?.nickNameBot || "GoatBot";
-    const prefix = global.GoatBot?.config?.prefix || "/";
-
-    // 🧠 TOTAL COMMANDS
+@@ -34,7 +34,7 @@ module.exports = {
     const totalCommands = global.GoatBot?.commands?.size || 0;
 
-    const images = [
-     "https://i.imgur.com/L5fGnYX.jpeg","https://i.imgur.com/mAifBNb.jpeg","https://i.imgur.com/Lf028tD.jpeg","https://i.imgur.com/CdzNRud.jpeg"
+    const images = ["https://i.imgur.com/L5fGnYX.jpeg","https://i.imgur.com/mAifBNb.jpeg","https://i.imgur.com/Lf028tD.jpeg","https://i.imgur.com/CdzNRud.jpeg"
+     
     ];
     const image = images[Math.floor(Math.random() * images.length)];
 
-    const now = moment().tz("Asia/Dhaka");
-    const date = now.format("MMMM Do YYYY");
-    const time = now.format("h:mm:ss A");
-
-    const uptime = process.uptime();
-    const days = Math.floor(uptime / 86400);
-    const hours = Math.floor((uptime % 86400) / 3600);
-    const minutes = Math.floor((uptime % 3600) / 60);
-    const seconds = Math.floor(uptime % 60);
-    const uptimeString = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-
-    return message.reply({
+@@ -53,18 +53,18 @@ module.exports = {
       body: `
 ╔═《 ✨ 𝗢𝗪𝗡𝗘𝗥 & 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 ✨ 》═╗
 
-⭓ 🤖 𝗕𝗼𝘁 𝗡𝗮𝗺𝗲  : 『 ${botName} 』
+⭓ 🤖 𝗕𝗼𝘁 𝗡𝗮𝗺𝗲   : 『 ${botName} 』
 ⭓ ☄️ 𝗣𝗿𝗲𝗳𝗶𝘅      : 『 ${prefix} 』
-⭓ 🧠 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀  : 『 ${totalCommands} 』
-⭓ ⚡ 𝗨𝗽𝘁𝗶𝗺𝗲     : 『 ${uptimeString} 』
-⭓ 🗓️ 𝗗𝗮𝘁𝗲       : 『 ${date} 』
-⭓ ⏰ 𝗧𝗶𝗺𝗲       : 『 ${time} 』
+⭓ 🧠 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀    : 『 ${totalCommands} 』
+⭓ ⚡ 𝗨𝗽𝘁𝗶𝗺𝗲      : 『 ${uptimeString} 』
+⭓ 🗓️ 𝗗𝗮𝘁𝗲        : 『 ${date} 』
+⭓ ⏰ 𝗧𝗶𝗺𝗲        : 『 ${time} 』
 
 ⭓ 👑 𝗢𝘄𝗻𝗲𝗿      : 『 ${ownerName} 』
 ⭓ 🎂 𝗔𝗴𝗲        : 『 ${ownerAge} 』
-⭓ ❤️ 𝗦𝘁𝗮𝘁𝘂𝘀      : 『 ${status} 』
-⭓ 📱 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿  : 『 ${ownerNumber} 』
-⭓ 🌐 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸   : 『 ${ownerFB} 』
+⭓ ❤️ 𝗦𝘁𝗮𝘁𝘂𝘀     : 『 ${status} 』
+⭓ 📱 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽  : 『 ${ownerNumber} 』
+⭓ 🌐 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸  : 『 ${ownerFB} 』
 
 ╚══════════════════════════╝
-`,
-      attachment: await global.utils.getStreamFromURL(image)
-    });
-  }
-};
