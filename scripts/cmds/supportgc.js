c45fc19095ceca36e9fc7d839d517c313b83ae64
Abrar Hasan Aadil <mdhojaifa550@gmail.com>
2026-01-19 13:21:08 +0600

    Update supportgc.js

--- scripts/cmds/supportgc.js
+++ scripts/cmds/supportgc.js
@@ -4,7 +4,7 @@ module.exports = {
     version: "1.1",
     author: "Shikaki",
     countDown: 5,
-    role: 0,
+    role: 2,
     shortDescription: {
       en: "Join the support group chat"
     },
