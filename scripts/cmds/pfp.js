619b6531db4c9066646d1b6aacd1317c5c8aca9c
Abrar Hasan Aadil <mdhojaifa550@gmail.com>
2026-03-05 00:38:10 +0600

    Update pfp.js

--- scripts/cmds/pfp.js
+++ scripts/cmds/pfp.js
@@ -5,7 +5,7 @@ const path = require("path");
 module.exports = {
         config: {
                 name: "pfp",
-                aliases: ["pp", "profilepic", "profile"],
+                aliases: ["profilepic", "profile"],
                 version: "1.0",
                 author: "NeoKEX",
                 countDown: 5,
