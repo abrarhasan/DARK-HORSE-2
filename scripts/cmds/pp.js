d94dcdd09c8c7f8aef13ce97562217f7c44a78ed
Abrar Hasan Aadil <mdhojaifa550@gmail.com>
2026-03-05 23:08:29 +0600

    Update pp.js

--- scripts/cmds/pp.js
+++ scripts/cmds/pp.js
@@ -1,37 +1,45 @@
-const { GoatWrapper } = require("fca-liane-utils");
+const axios = require("axios");
+const fs = require("fs-extra");
+const request = require("request");
 
 module.exports = {
   config: {
     name: "pp",
-    aliases: [],
-    version: "1.2",
-    author: "NIB (fixed by Mahbub Ullash)",
-    countDown: 5,
+    version: "1.0.0",
+    author: "abrar",
+    countDown: 3,
     role: 0,
-    shortDescription: "Facebook profile picture",
-    longDescription: "See anyone's Facebook profile picture (self/reply/mention/link).",
-    category: "image",
+    shortDescription: "Facebook profile 📸",
+    longDescription: "যেকোনো ইউজারের ফেসবুক প্রোফাইল পিকচার দেখা যাবে (নিজে, রিপ্লাই বা লিংক থেকে)।",
+    category: "media",
     guide: {
       en: "{pn} [reply/@mention/link]"
     }
   },
 
-  onStart: async function ({ api, event, args, message, usersData }) {
+  onStart: async function ({ api, event, args, usersData }) {
+    const cachePath = __dirname + "/cache/profile.png";
+
     try {
       let uid;
 
-      if (event.type === "message_reply" && event.messageReply?.senderID) {
+      // ১️⃣ রিপ্লাই করা মেসেজের ইউজারের প্রোফাইল
+      if (event.type === "message_reply") {
         uid = event.messageReply.senderID;
       }
 
-      else if (event.mentions && Object.keys(event.mentions).length > 0) {
+      // ২️⃣ @mention করা ইউজারের প্রোফাইল
+      else if (Object.keys(event.mentions || {}).length > 0) {
         uid = Object.keys(event.mentions)[0];
       }
 
+      // ৩️⃣ লিংক দেওয়া থাকলে
       else if (args[0] && args[0].includes(".com/")) {
-        uid = await api.getUID(args[0]);
+        const resID = await api.getUID(args[0]);
+        uid = resID;
       }
 
+      // ৪️⃣ কিছু না দিলে নিজের প্রোফাইল
       else {
         uid = event.senderID;
       }
@@ -40,15 +48,26 @@ module.exports = {
 
       const imageUrl = `https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
 
-      return message.reply({
-        body: ` `,
-        attachment: await global.utils.getStreamFromURL(imageUrl)
-      });
-    } catch (e) {
-      return message.reply("⚠️");
+      const callback = () => {
+        api.sendMessage(
+          {
+            body: `🌸✨Profile picture of ${name}  👇`,
+            attachment: fs.createReadStream(cachePath)
+          },
+          event.threadID,
+          () => fs.unlinkSync(cachePath),
+          event.messageID
+        );
+      };
+
+      request(encodeURI(imageUrl))
+        .pipe(fs.createWriteStream(cachePath))
+        .on("close", callback);
+
+    } catch (err) {
+      console.error(err);
+      api.sendMessage("⚠️ Something went wrong, Try again later.😭 ABRAR-BOT", event.threadID, event.messageID);
     }
   }
 };
 
-const wrapper = new GoatWrapper(module.exports);
-wrapper.applyNoPrefix({ allowPrefix: true });
