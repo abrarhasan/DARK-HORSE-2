1c8c2e51bc4d46dee859a88772fc0874d18282ad
Abrar Hasan Aadil <mdhojaifa550@gmail.com>
2026-03-06 21:17:42 +0600

    Update font.js

--- scripts/cmds/font.js
+++ scripts/cmds/font.js
@@ -203,7 +203,7 @@ module.exports = {
   config: {
     name: 'font',
     version: '1.0',
-    author: 'MR.AYAN',
+    author: 'abrar',
     countDown: 0,
     role: 0, // Set role to 0 for public access
     shortDescription: 'Convert text to different fonts',
